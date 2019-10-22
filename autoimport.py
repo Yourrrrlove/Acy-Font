f4f8bc5b94518906f39859dfb9b94ef7a31c72b3
lxfly2000 <gaoboyuan8@qq.com>
2019-10-23 07:58:32 +0800

    Update.

--- autoimport.py
+++ autoimport.py
@@ -1,24 +1,28 @@
 ﻿#python 3.7.3
 #encoding=utf-8
+#在 FontForge 中选择File,Execute Script...右键导入该脚本执行
 
-a=2500 #起始字符位置
-b=2999 #结束字符位置
+a=4500 #起始字符位置
+b=4999 #结束字符位置
 
 cx=20 #每行字符数量
 
 import fontforge
 
 font=fontforge.activeFont()
-for i in range(a,b):
+for i in range(a,b+1):
     glyph=font.createMappedChar(i)
-    if glyph.originalgid>=a and glyph.originalgid<b:
-        font.selection.select(glyph.originalgid)
-        #导入
-        y=int((glyph.originalgid-a)/cx)
-        x=glyph.originalgid-a-y*cx
-        img='Acy-Adobe-GB1-4-Regular-%d-%d_%02d_%02d.png'%(a,b,y,x)
-        glyph.importOutlines(img)
-        #提取字形
-        font.autoTrace()
-        #删除背景图像
-        #因 FontForge 未提供删除图像接口所以不要管它
+    font.selection.select(glyph.originalgid)
+    #导入
+    y=int((glyph.originalgid-a)/cx)
+    x=glyph.originalgid-a-y*cx
+    img='Acy-Adobe-GB1-4-Regular-%d-%d_%02d_%02d.png'%(a,b,y,x)
+    glyph.importOutlines(img)
+    #提取字形
+    font.autoTrace()
+    #删除背景图像
+    #因 FontForge 未提供删除图像接口所以需要执行完后手动删除
+
+#选择字形
+font.selection.select(("ranges",None),a,b)
+#之后需要手动选择Edit, Clear Background
