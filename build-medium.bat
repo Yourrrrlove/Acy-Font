46f812992be221e3bd7226d4189a5c975892c261
lxfly2000 <lxfly2000@outlook.com>
2021-08-28 17:15:40 +0800

    Fix wrong chars.

--- build-medium.bat
+++ build-medium.bat
@@ -1,7 +1,7 @@
 call afdko_env\Scripts\activate.bat
 if exist Medium\Acy-Medium.otf del Medium\Acy-Medium.otf
 if not exist Medium md Medium
-makeotf -f Acy-Medium.otf -ff ..\Adobe-GB1-20190729\GSUB\ag15-gsub.fea -fi fontinfo-medium.txt -r -nS -o Medium\Acy-Medium.otf
+makeotf -f Acy-Medium.otf -ff ..\Adobe-GB1\GSUB\ag15-gsub.fea -fi fontinfo-medium.txt -r -nS -o Medium\Acy-Medium.otf
 ttx -f Medium\Acy-Medium.otf
 notepad Medium\Acy-Medium.ttx
 ttx -f Medium\Acy-Medium.ttx
--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 call afdko_env\Scripts\activate.bat
 if exist Regular\Acy-Regular.otf del Regular\Acy-Regular.otf
 if not exist Regular md Regular
-makeotf -f Acy-Regular.otf -ff ..\Adobe-GB1-20190729\GSUB\ag15-gsub.fea -fi fontinfo.txt -r -nS -o Regular\Acy-Regular.otf
+makeotf -f Acy-Regular.otf -ff ..\Adobe-GB1\GSUB\ag15-gsub.fea -fi fontinfo.txt -r -nS -o Regular\Acy-Regular.otf
 ttx -f Regular\Acy-Regular.otf
 notepad Regular\Acy-Regular.ttx
 ttx -f Regular\Acy-Regular.ttx
