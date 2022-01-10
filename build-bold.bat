fefcc2e885a1adcd54dbf3712c0b410a0d4783a5
lxfly2000 <lxfly2000@outlook.com>
2022-01-10 17:22:11 +0800

    Update.

--- build-bold.bat
+++ build-bold.bat
@@ -1,4 +1,10 @@
-call afdko_env\Scripts\activate.bat
+if not exist afdko_env (
+	py -m venv afdko_env
+	call afdko_env\Scripts\activate.bat
+	py -m pip install afdko
+) else (
+	call afdko_env\Scripts\activate.bat
+)
 if exist Bold\Acy-Bold.otf del Bold\Acy-Bold.otf
 if not exist Bold md Bold
 makeotf -f Acy-Bold.otf -ff ..\Adobe-GB1\GSUB\ag15-gsub.fea -fi fontinfo-bold.txt -r -nS -o Bold\Acy-Bold.otf
--- build-medium.bat
+++ build-medium.bat
@@ -1,4 +1,10 @@
-call afdko_env\Scripts\activate.bat
+if not exist afdko_env (
+	py -m venv afdko_env
+	call afdko_env\Scripts\activate.bat
+	py -m pip install afdko
+) else (
+	call afdko_env\Scripts\activate.bat
+)
 if exist Medium\Acy-Medium.otf del Medium\Acy-Medium.otf
 if not exist Medium md Medium
 makeotf -f Acy-Medium.otf -ff ..\Adobe-GB1\GSUB\ag15-gsub.fea -fi fontinfo-medium.txt -r -nS -o Medium\Acy-Medium.otf
